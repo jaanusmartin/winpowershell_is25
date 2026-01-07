17a0cc66455b500794ec3b627d70130bd7f1353d
Jaanus Martin <jaanus.martin@voco.ee>
2026-01-07 14:40:47 +0200

    practice2

---
+++ practice2.ps1
@@ -0,0 +1,10 @@
+﻿# hashtable practice2
+$ht1 = @{name="John","Joe","Mary";DaysWorked=12,20,18}
+$ht2 = @{name=$ht1.name[0],$ht1.name[1],$ht1.name[2];SalaryPerDay=100,120,150}
+
+$JohnSalary = $ht1.DaysWorked[0] * $ht2.SalaryPerDay[0]
+$JoeSalary = $ht1.DaysWorked[1] * $ht2.SalaryPerDay[1]
+$MarySalary = $ht1.DaysWorked[2] * $ht2.SalaryPerDay[2]
+
+$ht3 = @{name=$ht1.name[0],$ht1.name[1],$ht1.name[2];Salary=$JohnSalary,$JoeSalary,$MarySalary}
+$ht3
