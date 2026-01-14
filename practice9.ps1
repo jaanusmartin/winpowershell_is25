439c3cce4cdf4f6437bd36ab2e4c37610af0d579
Jaanus Martin <jaanus.martin@voco.ee>
2026-01-14 14:49:00 +0200

    practice 9

---
+++ practice9.ps1
@@ -0,0 +1,27 @@
+﻿# skript mis teavitab et notepad töötab kuni notepad töötab
+
+$loendur1 = 0
+
+do
+{
+    $kontroll = $null
+    $kontroll = Get-Process | ?{$_.Name -like "note*"}
+    
+    if( $kontroll -ne $null )
+        {
+            $loendur2 = 2
+            Write-Host "Notepad töötab"
+            Start-Sleep -Seconds 1
+            $loendur1++
+        }
+    
+    else
+        {
+            $loendur2 = 1
+         }
+
+}
+while( $loendur2 -ne 1 )
+
+$loendur1
+    
