3e2340581d079fa4cb9b021fdc244b96ce966cd2
Jaanus Martin <jaanus.martin@voco.ee>
2026-01-14 12:56:17 +0200

    practice 7

---
+++ practice7.ps1
@@ -0,0 +1,16 @@
+﻿# skript mis annab 20-le järjekorranumbrile suvalise värvi valikust
+# (punane, roheline, kollane, sinine)
+
+$varvid = @("punane","roheline","kollane","sinine")
+$jagatudvarvid = @()
+
+for( $jknumber = 1 ; $jknumber -le 20 ; $jknumber++ )
+    {
+        $suvalinevarv = Get-Random $varvid
+        $temp = [PSCustomObject]@{
+                                    JKNumber = $jknumber
+                                    Värv = $suvalinevarv
+                                  }
+        $jagatudvarvid += $temp
+    }
+$jagatudvarvid
