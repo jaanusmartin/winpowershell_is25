1f0a9bf68e36076c92ba8c73d04657c75cb2fe5d
Jaanus Martin <jaanus.martin@voco.ee>
2026-01-14 13:42:06 +0200

    practice 8

---
+++ practice8.ps1
@@ -0,0 +1,8 @@
+﻿# skript mis kontrollib kas notepad töötab või mitte ja kuvab sellekohase
+# teavituse ekraanile
+
+while( Get-Process Notepad -ErrorAction SilentlyContinue )
+    {
+        Write-Host "Notepad töötab."
+        Start-Sleep -Seconds 1
+     }
