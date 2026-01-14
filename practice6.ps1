3b0e37b22f33b25bcf987f5ae1aa6a9f28c37ce5
Jaanus Martin <jaanus.martin@voco.ee>
2026-01-14 12:31:30 +0200

    practice 6

---
+++ practice6.ps1
@@ -0,0 +1,23 @@
+﻿# skript mis võtab failist laste nimed ja vanused
+# vanuse alusel ütleb kas ta on alg- või põhikoolis ja teeb sellest eraldi massiivi
+# 4-10 algkool  11-17 põhikool
+# loodud massivi salvestab CSV faili arvutis
+
+$kontroll = @()
+$lapsed = Import-Csv C:\Users\Jaanus\powershellscript\vanused.csv
+
+foreach ( $l in $lapsed )
+    { if( [int]$l.Vanus -ge 4 -and  [int]$l.Vanus -le 10 )
+        { $kool = "algkool"
+        }
+    elseif( [int]$l.Vanus -ge 11 -and [int]$l.Vanus -le 17 )
+        { $kool = "põhikool"
+        }
+    $temp = [PSCustomObject]@{
+            Nimi = $l.Nimi
+            Kool = $kool
+            }
+    $kontroll += $temp
+    }
+$kontroll
+$kontroll | Export-Csv -Path "C:\Users\Jaanus\powershellscript\sorteeritud-koolide-lõikes.csv" -NoTypeInformation
