b33ee1592e8bfa1e81edb576e69f6eed8491a502
Jaanus Martin <jaanus.martin@voco.ee>
2026-01-19 11:45:01 +0200

    practice 5.2

---
+++ practice5.2.ps1
@@ -0,0 +1,11 @@
+﻿# skript mis näitab kausta c:\temp\test sisu ja otsib sealt välja .csv faild
+# ning kuvab nende suuruse kilobaitides ja megabaitides
+
+Get-ChildItem -Path "C:\temp\test"
+$file = Get-ChildItem -Path "C:\temp\test" | where {$_.Name -like "*.csv"} | select Name,Length
+$suurusKB = $file.Length/1KB
+$suurusMB = $file.Length/1MB
+
+Write-Host "`nFailiNimi : "$file.Name
+Write-Host "Suurus kilobaitides : "$suurusKB
+Write-Host "Suurus megabaitides : "$suurusMB
