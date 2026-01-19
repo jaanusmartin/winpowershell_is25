867b786ce264fd41c2168b40d59233bf2aaf44d6
Jaanus Martin <jaanus.martin@voco.ee>
2026-01-19 19:19:30 +0200

    practice 12.1

---
+++ practice12.1.ps1
@@ -0,0 +1,17 @@
+﻿# funktsioon ilma parameetriteta mis tagastab töötavate ja peatatud teenuste arvu
+
+function tootavad-teenused
+    {
+        $tootavad = Get-Service | ?{$_.Status -eq "Running"}
+        Write-Host "Töötavaid teenuseid on"$tootavad.count
+     }
+
+function seisvad-teenused
+    {
+        $seisvad = Get-Service | ?{$_.Status -eq "Stopped"}
+        Write-Host "Seisvaid teenuseid on"$seisvad.count
+     }
+
+tootavad-teenused
+
+seisvad-teenused
