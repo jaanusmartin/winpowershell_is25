c79712a6d990731ec26d1a4cf3c1b1c9914cfd9b
Jaanus Martin <jaanus.martin@voco.ee>
2026-01-19 19:30:50 +0200

    practice 12.2

---
+++ practice12.2.ps1
@@ -0,0 +1,22 @@
+﻿
+
+function loenda-teenused
+    {
+        param
+            (
+                [string]$status
+             )
+        if( $status -eq "Running")
+            {
+                $tootav = Get-Service | ?{$_.Status -eq "Running"}
+                Write-Host "Töötavate teenuste arv on"$tootav.count
+             }
+        elseif( $status -eq "Stopped" )
+            {
+                $peatatud = Get-Service | ?{$_.Status -eq "Stopped"}
+                Write-Host "Seisvaid teenuseid on"$peatatud.count
+             }
+     }
+
+loenda-teenused -status Running
+loenda-teenused -status Stopped
